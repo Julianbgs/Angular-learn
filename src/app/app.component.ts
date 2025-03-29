3040dc18be8a94c815ac13064638bd25c51844dd
Julian Blagis <julian.blazhis@brand-monitor.com>
2025-03-29 17:47:15 +0300

    first lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from '../core/services/api.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-cli';
+  data: any = [];
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+    this.apiService.getApiTools('https://jsonplaceholder.typicode.com/posts')
+      .subscribe((res) => {
+      this.data = res;
+    })
+  }
 }
